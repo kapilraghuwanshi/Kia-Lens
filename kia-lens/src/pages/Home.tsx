9948e21ed6218d969b25ce5ca8be4a972738c184
Kapil Raghuwanshi <kraghuwanshi@conversantmedia.com>
2021-03-16 17:15:23 +0530

    Added camera module, logo and android starter

--- kia-lens/src/pages/Home.tsx
+++ kia-lens/src/pages/Home.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Camera, CameraOptions  } from '@ionic-native/camera';
 import './Home.css';
+import React from 'react';
 
 const Home: React.FC = () => {
+  const cameraOpts : CameraOptions = {
+    quality: 100,
+    destinationType: Camera.DestinationType.FILE_URI,
+    encodingType: Camera.EncodingType.JPEG,
+    mediaType: Camera.MediaType.PICTURE
+  }
+  const openCamera = async () => {
+    const data = await Camera.getPicture(cameraOpts);
+    console.log(`Camera Results: ${data.text}`);
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Kia Lens</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonButton onClick={openCamera}>Open Camera</IonButton>
       </IonContent>
     </IonPage>
   );
